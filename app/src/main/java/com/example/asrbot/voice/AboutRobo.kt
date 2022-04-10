ae2996af8930a3851acd6409b6e097958f4506ba
14920251 <85061916+14920251@users.noreply.github.com>
2022-04-11 00:48:41 +0530

    first

---
+++ app/src/main/java/com/example/asrbot/voice/AboutRobo.kt
@@ -0,0 +1,12 @@
+package com.example.asrbot.voice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.asrbot.R
+
+class AboutRobo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_robo)
+    }
+}
